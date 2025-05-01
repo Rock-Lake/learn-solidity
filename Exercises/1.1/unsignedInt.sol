fc0ace2d00d8dd32c3f299b026a9c760b433acd8
Roba Chimdesa <130156480+RobaChimdesa@users.noreply.github.com>
2025-05-01 18:06:03 +0300

    Update unsignedInt.sol

--- Exercises/1.1/unsignedInt.sol
+++ Exercises/1.1/unsignedInt.sol
@@ -6,3 +6,23 @@ contract Contract {
     uint16 public b = 260;
     uint public sum = a + b;
 }
+
+
+// by Roba
+
+Unsigned Int Constructor
+Create a constructor which will take a uint as an argument.
+Store this uint value inside a public storage variable called x.
+
+// src/Contract.sol
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+contract Contract {
+    uint public x;
+
+    // Constructor to set initial value of x
+    constructor(uint initialX) {
+        x = initialX;
+    }
+}
