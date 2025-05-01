62200507aa0a070906a8d0319ee49e0ed32f35b0
Roba Chimdesa <130156480+RobaChimdesa@users.noreply.github.com>
2025-05-01 18:12:38 +0300

    Update double.sol

--- Exercises/1.2/double.sol
+++ Exercises/1.2/double.sol
@@ -6,3 +6,19 @@ contract Contract {
         return 2*x;
     }
 }
+
+
+// roba
+
+Create an external, pure function called double which takes a uint parameter and doubles it.
+It should return this doubled uint value.
+
+// src/Contract.sol
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+contract Contract {
+    function double(uint num) public pure returns (uint) {
+        return num * 2;
+    }
+}
